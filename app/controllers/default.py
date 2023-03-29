abc6723dbc816ca6bcb2a33a2068697931c414d5
Lucas Linhares <lucasvcl2008@hotmail.com>
2023-03-29 10:01:43 -0300

    ups

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -31,7 +31,8 @@ def controle_lead():
     if(len(leads) <= 1):
         return gera_response(200, 'return', {}, 'Sem duplicatos')
     merge = b24.merge()
-    return gera_response(200, 'return', {merge['result']}, 'Sem duplicatos')
+    
+    return gera_response(200, 'return', merge['result']['STATUS'], 'Sem duplicatos')
 
 def gera_response(status, nome_conteudo, conteudo, mensagem=False):
     body = {}
